33e01a2b14208e64a6f7c3ddc58fa6dc87a4633e
Alby Joseph <albysoy1@gmail.com>
2026-08-24 16:08:58 -0500

    added support for Vercel Speed Insights and Analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Layout from './components/Layout';
 import Standings from './pages/Standings';
 import RacePace from './pages/RacePace';
@@ -16,6 +18,8 @@ function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Layout>
+      <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   );
 }
